57318e791827adedc46290579704154ad800e1f3
aquenchonline <aquenchonline@gmail.com>
2025-12-14 13:48:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import numpy as np
 # ------------------------------------------------------------------
 st.set_page_config(
     page_title="Amavik ERP", 
-    layout="wide", 
+    layout="boxed", 
     page_icon="🏭",
     initial_sidebar_state="collapsed"
 )
